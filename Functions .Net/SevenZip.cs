0e053c5a7b9c985ade153a50a0eba18506941c8d
Charles-Henri Sauget <contact@sauget-ch.fr>
2021-09-16 07:42:08 +0200

    7z

--- Functions .Net/SevenZip.cs
+++ Functions .Net/SevenZip.cs
@@ -15,45 +15,43 @@ using Azure.Storage.Files.DataLake.Models;
 using Azure.Storage;
 using Microsoft.Azure.Services.AppAuthentication;
 using Azure.Identity;
+using System.Web;
 
 namespace Company.Function
 {
-    //https://icsharpcode.github.io/SharpZipLib/help/api/ICSharpCode.SharpZipLib.BZip2.BZip2.html
-    //https://www.oreilly.com/library/view/windows-developer-power/0596527543/ch04s11.html
+
     public static class SevenZip
     {
         [FunctionName("SevenZip")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "SevenZip/{account}/{container}/{directory}/{filename}")] HttpRequest req,
+            string account, string container, string directory, string filename, ILogger log)
         {
             
             //Retrieve File from storage
-            var lakeClient = GetDataLakeServiceClient("synapselakeapp");
-            var fileSystemClient = lakeClient.GetFileSystemClient("lake");
-            DataLakeDirectoryClient directoryClient = fileSystemClient.GetDirectoryClient("StackOverflow/raw/");
-            var file = directoryClient.GetFileClient("stackoverflow.com-PostHistory.7z");
-            System.IO.Stream sr = (await file.ReadAsync()).Value.Content;
-            
-            using (var fileStream = File.Create(@"D:\GIT\complete-big-data-architecture\Functions .Net\test.xml"))
+            var lakeClient = GetDataLakeServiceClient(HttpUtility.UrlDecode(account));
+            var fileSystemClient = lakeClient.GetFileSystemClient(HttpUtility.UrlDecode(container));
+            DataLakeDirectoryClient directoryClient = fileSystemClient.GetDirectoryClient(HttpUtility.UrlDecode(directory));
+
+            var DownloadFile = directoryClient.GetFileClient(HttpUtility.UrlDecode(filename));
+            var ReadStream = await DownloadFile.OpenReadAsync();
+            var response = req.HttpContext.Response;
+            response.StatusCode = 200;
+            response.ContentType = "application/json-data-stream";
+            using (var archive = SevenZipArchive.Open(ReadStream, null))
             {
-                using (var archive = SevenZipArchive.Open(sr, null))
+                foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
                 {
-                    foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
+                    
+                    foreach (IArchiveEntry e in archive.Entries)
                     {
-
-                        foreach (IArchiveEntry e in archive.Entries)
-                        {
-                            e.WriteTo(fileStream);
-                        }
-
+                        
+                        e.WriteTo(response.Body);                  
                     }
+
                 }
             }
-                
-            
-            
-            return new OkObjectResult("ok");
+            return new EmptyResult();
         }
         public static DataLakeServiceClient GetDataLakeServiceClient(string accountName)
         {
